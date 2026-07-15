eec9e9c0b8ce857d46d5ea40f46c67d00b289f58
anand-aman <itsamananand@gmail.com>
2026-07-16 00:08:07 +0530

    spring initialize and dependency setup

---
+++ src/main/java/com/curiodesk/curiogo/CurioGoApplication.java
@@ -0,0 +1,11 @@
+package com.curiodesk.curiogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurioGoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CurioGoApplication.class, args);
+    }
+}
